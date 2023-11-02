662e9c6e56b63f90705a03ac2080cdffb380a46b
Matheus <129191137+matheusmg08@users.noreply.github.com>
2023-11-02 16:48:43 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,26 @@
+function voltar(){
+    var image = document.getElementById ("camisaazul");
+
+    image.src = "camiseta1.costa.jpg"
+}
+function avancar(){
+    var image1 = document.getElementById ("camisaazul");
+    image1.src = "camiseta1.frente.jpg"
+}
+function camisa1(){
+    var image2 = document.getElementById ("camisaazul");
+    image2.src = "camiseta1.frente.jpg"
+}
+function camisa2(){
+    var image3 = document.getElementById ("camisaazul");
+    image3.src = "camisetaverde.frente.jpg"
+}
+function voltar1(){
+    var image = document.getElementById ("camisaazul");
+
+    image.src = "camisetaverde.costa.jpg"
+}
+function avancar2(){
+    var image1 = document.getElementById ("camisaazul");
+    image1.src = "camisetaverde.frente.jpg"
+}
